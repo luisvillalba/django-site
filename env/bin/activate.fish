3652d6b18031fc57e4658d1697244a8a0803cc0a
Luis Villalba <luismvillalba@gmail.com>
2018-10-17 18:41:18 -0500

    Fixing security issues

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/lmazzey/code/github/django-site/site"
+set -gx VIRTUAL_ENV "/Users/lmazzey/code/github/django-site/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
